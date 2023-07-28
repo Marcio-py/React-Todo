f765c743370f120c8a78ddbfaa0727754291bedb
unknown <ivan.piceno171@gmail.com>
2023-07-27 19:26:26 -0700

    fix toggle

--- Mod4/src/Components/TodoItem.jsx
+++ Mod4/src/Components/TodoItem.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 // export default function TodoItem({completed, id, title, toggleTodos, deleteTodos}) {
 export default function TodoItem({completed, id, title, toggleTodos, deleteTodos}) {
+    
   return (
     <div>
        <li key={id}>
